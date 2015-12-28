71b0900f1300404953e03694aed81dd7d558bf29
Christian Loos <chris@crt0.org>
2015-12-28 14:08:16 +0100

    reconfigured project

--- src/tunefish4/JuceLibraryCode/AppConfig.h
+++ src/tunefish4/JuceLibraryCode/AppConfig.h
@@ -337,7 +337,7 @@
  #define JucePlugin_MaxNumOutputChannels   2
 #endif
 #ifndef  JucePlugin_PreferredChannelConfigurations
- #define JucePlugin_PreferredChannelConfigurations  {1, 1}, {2, 2}
+ #define JucePlugin_PreferredChannelConfigurations  {2, 2}
 #endif
 
 #endif  // __JUCE_APPCONFIG_HMEUE0__
