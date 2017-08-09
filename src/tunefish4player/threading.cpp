848dfbede479064ba99c7909b2a12a8c7b8d6ecd
Christian Loos <chris@crt0.org>
2017-08-09 17:23:06 +0200

    headers changed

--- src/tunefish4player/threading.cpp
+++ src/tunefish4player/threading.cpp
@@ -1,16 +1,23 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- *   This file is part of
- *       ______      _                          ______
- *      / ____/___  (_)___ _____ ___  ____ _   / ____/
- *     / __/ / __ \/ / __ `/ __ `__ \/ __ `/  /___ \
- *    / /___/ / / / / /_/ / / / / / / /_/ /  ____/ /
- *   /_____/_/ /_/_/\__, /_/ /_/ /_/\__,_/  /_____/.
- *                 /____/
- *
- *   Copyright © 2003-2014 Brain Control, all rights reserved.
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*
+---------------------------------------------------------------------
+Tunefish 4  -  http://tunefish-synth.com
+---------------------------------------------------------------------
+This file is part of Tunefish.
+
+Tunefish is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Tunefish is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Tunefish.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------
+*/
 
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
