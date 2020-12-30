ab30109dc9da088db394de76c213f0848e90f197
Christian Loos <chris@crt0.org>
2020-12-30 13:21:42 +0100

    Added dmg creation

--- scripts/buildReleaseTf4_Mac.sh
+++ scripts/buildReleaseTf4_Mac.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir ../artifacts
 mkdir ../build
+mkdir ../build/dmg
 cd ../build
 
 cp -R ~/Library/Audio/Plug-Ins/Components/Tunefish4.component .
@@ -13,6 +14,12 @@ cp ../COPYING .
 zip -9 -r ../artifacts/tunefish4-$(cat ../FULLVERSION_TF4).zip *
 
 packagesbuild ../installers/tf4_mac/Tunefish\ 4.pkgproj
-cp ../installers/tf4_mac/build/Tunefish4.pkg ../artifacts/tunefish4-$(cat ../FULLVERSION_TF4).pkg
+cp ../installers/tf4_mac/build/Tunefish4.pkg ../build/dmg/tunefish4-$(cat ../FULLVERSION_TF4).pkg
+cp ../README.txt ../build/dmg
+cp ../CHANGES.txt ../build/dmg
+cp ../COPYING ../build/dmg
+
+hdiutil create ../build/tmp.dmg -ov -volname "Tunefish4" -fs HFS+ -srcfolder ../build/dmg
+hdiutil convert ../build/tmp.dmg -format UDZO -o ../artifacts/tunefish4-$(cat ../FULLVERSION_TF4).dmg
 
 cd ../scripts
